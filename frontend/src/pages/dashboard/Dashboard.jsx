4f6a1a693dad7fa66e92de5cb8551f26ba12c510
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-09-12 10:36:39 -0300

    feat: adiciona arquivo vazio de dashboard

---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Row } from 'antd';
+
+export default function Dashboard() {
+  return (
+    <Row>
+    </Row>
+  );
+}
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,17 +1,25 @@
 import React, { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const Login = lazy(() => import('../login/Login'));
+const PrivateRoute = lazy(() => import('../components/PrivateRoute'));
+
+const Login = lazy(() => import('../pages/login/Login'));
+const Dashboard = lazy(() => import('../pages/dashboard/Dashboard'));
 
 export default function AppRouter() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route index
           element={<Login />} />
-        <Route path='/login'
+        <Route path='/entrar'
           element={<Login />} />
+        <Route element={<PrivateRoute />}>
+          <Route index
+            element={<Dashboard />} />
+          <Route path='/dashboard'
+            element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
