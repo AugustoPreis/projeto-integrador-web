e64e20bae39ffda8178a96dd0bee5254fd796995
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-09-12 15:16:24 -0300

    refactor: altera estrutura do componente de requisicao http

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,76 +1,90 @@
 import axios from 'axios';
+import { getToken, logout } from '../providers/AuthProvider';
 
 export default function request(url, context) {
-  context.url = url;
+  const normalizedContext = normalizeContext(url, context);
+  let httpRequest;
 
-  if (typeof context.method != 'string') {
-    context.method = 'GET';
-  }
-
-  if (!['GET', 'POST', 'PUT', 'DELETE'].includes(context.method.toUpperCase())) {
-    context.method = 'GET';
-  }
-
-  switch (context.method.toUpperCase()) {
+  switch (context.method) {
     case 'GET':
-      return get(context);
+      httpRequest = get(normalizedContext);
+      break;
     case 'POST':
-      return post(context);
+      httpRequest = post(normalizedContext);
+      break;
     case 'PUT':
-      return put(context);
+      httpRequest = put(normalizedContext);
+      break;
     case 'DELETE':
-      return _delete(context);
+      httpRequest = _delete(normalizedContext);
+      break;
   }
+
+  return new Promise((resolve, reject) => {
+    httpRequest
+      .then((response) => {
+        resolve(response.data);
+      }).catch((err) => {
+        if (err.response.status === 401) {
+          logout();
+        }
+
+        reject(err.response.data);
+      });
+  });
 }
 
 function get(context) {
-  return new Promise((resolve, reject) => {
-    axios.get(context.url, {
-      headers: context.headers,
-      params: context.params,
-    }).then((response) => {
-      resolve(response.data);
-    }).catch((err) => {
-      reject(err.response.data);
-    });
+  return axios.get(context.url, {
+    headers: context.headers,
+    params: context.params,
   });
 }
 
 function post(context) {
-  return new Promise((resolve, reject) => {
-    axios.post(context.url, context.body, {
-      headers: context.headers,
-      params: context.params,
-    }).then((response) => {
-      resolve(response.data);
-    }).catch((err) => {
-      reject(err.response.data);
-    });
+  return axios.post(context.url, context.body, {
+    headers: context.headers,
+    params: context.params,
   });
 }
 
 function put(context) {
-  return new Promise((resolve, reject) => {
-    axios.put(context.url, context.body, {
-      headers: context.headers,
-      params: context.params,
-    }).then((response) => {
-      resolve(response.data);
-    }).catch((err) => {
-      reject(err.response.data);
-    });
+  return axios.put(context.url, context.body, {
+    headers: context.headers,
+    params: context.params,
   });
 }
 
 function _delete(context) {
-  return new Promise((resolve, reject) => {
-    axios.delete(context.url, {
-      headers: context.headers,
-      params: context.params,
-    }).then((response) => {
-      resolve(response.data);
-    }).catch((err) => {
-      reject(err.response.data);
-    });
+  return axios.delete(context.url, {
+    headers: context.headers,
+    params: context.params,
   });
+}
+
+function normalizeContext(url, dirt) {
+  const context = { ...dirt };
+
+  context.method = context.method?.toUpperCase();
+  context.url = url;
+
+  if (!['GET', 'POST', 'PUT', 'DELETE'].includes(context.method)) {
+    context.method = 'GET';
+  }
+
+  if (!context.headers) {
+    context.headers = {};
+  }
+
+  if (context.api !== false) {
+    const token = getToken();
+
+    context.url = `/api${context.url}`;
+
+    if (token) {
+      context.headers['Authorization'] = token;
+    }
+  }
+
+  return context;
 }
