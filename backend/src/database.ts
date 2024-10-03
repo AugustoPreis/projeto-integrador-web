69aec0c08373dd70501033f35e08e0cd6117981a
Augusto Preis Tomasi <augusto.116122@alunosatc.edu.br>
2024-10-03 19:55:50 -0300

    alteracoes para apresentar o projeto

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -5,7 +5,19 @@ import { configDB } from './config/database';
 
 pg.types.setTypeParser(pg.types.builtins.NUMERIC, Number);
 
-export const Database = new DataSource(configDB());
+const { env } = process;
+
+export const Database = new DataSource({
+	host: env.DB_HOST,
+	port: Number(env.DB_PORT),
+	username: env.DB_USER,
+	password: env.DB_PASS,
+	database: env.DB_NAME,
+	type: 'postgres',
+	synchronize: false,
+	logging: env.LOG_SQL === 'TRUE',
+	entities: [`${__dirname}/models/**.{js,ts}`],
+});
 
 export async function getQueryRunner(start = true): Promise<QueryRunner> {
 	const qr = Database.createQueryRunner();
