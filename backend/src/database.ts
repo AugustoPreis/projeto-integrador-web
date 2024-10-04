dba8e3025dc4cf4f2cb94c93e24267b48ee68dd2
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-10-04 08:48:29 -0300

    volta configuracao antiga do projeto

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -5,19 +5,7 @@ import { configDB } from './config/database';
 
 pg.types.setTypeParser(pg.types.builtins.NUMERIC, Number);
 
-const { env } = process;
-
-export const Database = new DataSource({
-	host: env.DB_HOST,
-	port: Number(env.DB_PORT),
-	username: env.DB_USER,
-	password: env.DB_PASS,
-	database: env.DB_NAME,
-	type: 'postgres',
-	synchronize: false,
-	logging: env.LOG_SQL === 'TRUE',
-	entities: [`${__dirname}/models/**.{js,ts}`],
-});
+export const Database = new DataSource(configDB());
 
 export async function getQueryRunner(start = true): Promise<QueryRunner> {
 	const qr = Database.createQueryRunner();
